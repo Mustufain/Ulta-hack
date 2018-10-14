21b8a1876a1a56421e00f8a74cedf9dace0ff57d
jhkoivis <juhakoivisto@outlook.com>
2018-10-14 09:46:28 +0300

    cleaned upo code

--- hand_analyzer.py
+++ hand_analyzer.py
@@ -1,4 +1,14 @@
 
+#####################################
+# Hand Analyzer                     #
+#####################################
+#
+# This file analyzes the cleanliness
+# of the hands
+#
+#####################################
+
+# import libraries
 import cv2
 import numpy
 import time
@@ -6,11 +16,11 @@ import playsound
 
 class Hand_analyzer:
     
-    
     def __init__(self):
-        
-        self.dirt_threshold = 35 #40 #40
-        self.beep_thres     = 1000 #2100000 #2000000 #7000000#30000000 #30000000000000000000000000 #30000000
+    
+        # set parameters for dirt and beep (if enabled)
+        self.dirt_threshold = 35 
+        self.beep_thres     = 1000
 
         self.bg_ok       = cv2.imread('software_image_ok.png',1)
         self.bg_bad      = cv2.imread('software_image_bad.png',1) 
@@ -37,49 +47,20 @@ class Hand_analyzer:
     
         red_pixels = numpy.sum(filtered[50:,:,2])
     
-        print(red_pixels)
-    
+        # if threshold is exceeded return thresholded image and set is_dirty flag to True
         if red_pixels > self.beep_thres: 
-
+            
+            # uncomment these lines if you want a beep
             #current_time = time.time()
             #if current_time > self.start_time + 1: 
             #    playsound.playsound('haha.mp3', 0)
             #    self.start_time = current_time
-         
-         
+      
             return (filtered, True)   
         
+        # if threshold is not exceeded, return thresholded image and set is_dirty flag to False
         else:
             
             return (filtered, False)
-              
-            #bg_bad[150: 150 + 240, 
-            #       100: 100 + 320, :]           = img_BGRA[ymin:ymax:ystep, xmin:xmax:xstep, :]
-            
-            #bg_bad[150: 150 + ymax/ystep,
-            #       600: 600 + xmax/ystep, :]    = frame2[ymin:ymax:ystep, 
-            #                                             xmin:xmax:xstep, :]
-        
-            #cv2.imshow('frame 2', frame2)
-        
-            #cv2.imshow('bg', bg_bad)
-        
-    #else:
-        
-        #bg_ok[150: 150 + 240, 
-        #       100: 100 + 320, :]       = img_BGRA[ymin:ymax:ystep, xmin:xmax:xstep, :]
-        
-        #bg_ok[150: 150 + ymax/ystep,
-        #       600: 600 + xmax/ystep, :]    = frame2[ymin:ymax:ystep, 
-        #                                             xmin:xmax:xstep, :]
-    
-        #cv2.imshow('frame 2', frame2)
-    
-        #cv2.imshow('bg', bg_ok)
-        
-        
-        
-        
-        
-        
+           
         
